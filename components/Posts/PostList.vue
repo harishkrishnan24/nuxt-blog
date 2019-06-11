af72bc4011346a03d5ce90e4fc17385ecad1f6ef
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-06-11 14:08:52 +0530

    Add error page and other layouts

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -3,18 +3,21 @@
     <section class="post-list">
       <PostPreview
         id="1"
+        :is-admin="isAdmin"
         thumbnail="https://www.itechpeeps.com/wp-content/uploads/2019/04/grid-ai.png"
         title="Hello there!"
         previewText="This is my first post!"
       />
       <PostPreview
         id="2"
+        :is-admin="isAdmin"
         thumbnail="https://www.itechpeeps.com/wp-content/uploads/2019/04/grid-ai.png"
         title="Hello there2!"
         previewText="This is my second post!"
       />
       <PostPreview
         id="3"
+        :is-admin="isAdmin"
         thumbnail="https://www.itechpeeps.com/wp-content/uploads/2019/04/grid-ai.png"
         title="Hello there3!"
         previewText="This is my third post!"
@@ -28,6 +31,12 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 }
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Harish',
+        title: 'Nuxt Js Intro',
+        content: 'Best VueJs course',
+        thumbnailLink:
+          'https://www.itechpeeps.com/wp-content/uploads/2019/04/grid-ai.png'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
+
+
